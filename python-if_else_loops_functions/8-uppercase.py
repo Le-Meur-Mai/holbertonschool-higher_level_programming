5e0088afae2a94454afd7edc8125df8b7569ef6d
Le-Meur-Mai <mai.lm@outlook.fr>
2025-09-02 14:23:58 +0200

    Deleting parasyte spaces

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,11 +2,11 @@
 
 def uppercase(str):
     result = ""
-    for i in range (len(str)):
+    for i in range(len(str)):
         j = str[i]
         code_ascii = ord(j)
         if code_ascii >= 97 and code_ascii < 123:
             code_ascii -= 32
         result += chr(code_ascii)
-    
+
     print("{}".format(result))
