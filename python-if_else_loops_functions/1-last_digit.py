a0c94410a3af1debd19a4ebc53a46e5284e6702b
Le-Meur-Mai <mai.lm@outlook.fr>
2025-09-02 10:12:35 +0200

    Correction of the python codding style

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -10,7 +10,7 @@ if number < 0:
 else:
     last_number = number % 10
 
-print(f"Last digit of {number} is {last_number}", end = " ")
+print(f"Last digit of {number} is {last_number}", end=" ")
 
 if last_number > 5:
     print("and is greater than 5")
