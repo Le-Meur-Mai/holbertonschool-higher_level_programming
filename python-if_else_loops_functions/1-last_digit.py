ec503255183b13111cdef9dd00b94ed4a569a862
Le-Meur-Mai <mai.lm@outlook.fr>
2025-09-02 10:05:43 +0200

    Correcting the code if the number is negative

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_number = number % 10
+last_number = 0
+
+if number < 0:
+    last_number = number * -1
+    last_number = last_number % 10
+else:
+    last_number = number % 10
 
 print(f"Last digit of {number} is {last_number}", end = " ")
 
