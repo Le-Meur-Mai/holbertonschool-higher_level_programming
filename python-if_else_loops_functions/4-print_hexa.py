7365563ad505341fb39d368a7e297067785da3b3
Le-Meur-Mai <mai.lm@outlook.fr>
2025-09-02 11:45:17 +0200

    Correcting the code python style

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-for i in range(0,99):
+for i in range(0, 99):
     print("{} = {num}".format(i, num=hex(i)))
--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,11 +3,11 @@ i = 0
 j = 1
 k = 1
 while i < 9:
-    while j <=9:
+    while j <= 9:
         if i != 8:
             print("{}{}, ".format(i, j), end="")
         else:
-            print("{}{}".format(i,j))
+            print("{}{}".format(i, j))
         j += 1
     i += 1
     k += 1
