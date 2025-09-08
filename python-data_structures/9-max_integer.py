ffdcf95b0742bb3cc7d6c2635c7a5982d5b4429c
Le-Meur-Mai <mai.lm@outlook.fr>
2025-09-08 14:52:06 +0200

    Correcting the code that was returning 0 if all numbers  were negative

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    max_number = 0    
     if len(my_list) == 0:
         return
     else:
+        max_number = my_list[0]
         for i in range(0, len(my_list)):
             if my_list[i] > max_number:
                 max_number = my_list[i]
