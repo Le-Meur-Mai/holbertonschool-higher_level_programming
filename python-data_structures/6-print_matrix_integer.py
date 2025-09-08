3a8fb6651d759f78d881aedd754c6db62b706b71
Le-Meur-Mai <mai.lm@outlook.fr>
2025-09-08 14:31:00 +0200

    Adding a function that add two integers of two tuples

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if matrix != None:
+    if matrix is not None:
         for i in range(0, len(matrix)):
             for j in range(0, len(matrix[i])):
                 print("{:d}".format(matrix[i][j]), end="")
---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    new_list = []
+    for i in range(0, 2):
+        if i < len(tuple_a):
+            new_list.append(tuple_a[i])
+        else:
+            new_list.append(0)
+    for i in range(0, 2):
+        if i < len(tuple_b):
+            new_list.append(tuple_b[i])
+        else:
+            new_list.append(0)
+            
+    new_tuple = ((new_list[0] + new_list[2]), (new_list[1] + new_list[3]))
+    return (new_tuple)
