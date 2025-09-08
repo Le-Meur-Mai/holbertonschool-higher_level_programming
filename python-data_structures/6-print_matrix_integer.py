40aa8cc0955e4e0d679b0572a0a68da7f63dec2d
Le-Meur-Mai <mai.lm@outlook.fr>
2025-09-08 11:49:05 +0200

    Adding the last modification

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     if matrix != None:
         for i in range(0, len(matrix)):
             for j in range(0, len(matrix[i])):
-                print("{}".format(matrix[i][j]), end="")
+                print("{:d}".format(matrix[i][j]), end="")
                 if j != (len(matrix[i]) - 1):
                     print(" ", end="")
             print("")
