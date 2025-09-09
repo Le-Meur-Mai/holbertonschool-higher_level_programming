84f4e341b8d2d414c09a2837cab34e0166257562
Le-Meur-Mai <mai.lm@outlook.fr>
2025-09-09 11:16:45 +0200

    Adding a function that return the key that have the highest value

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return
+    else:
+        score = 0
+        for i in a_dictionary:
+            if a_dictionary[i] > score:
+                score = a_dictionary[i]
+        for i in a_dictionary:
+            if a_dictionary[i] == score:
+                best_student = i
+                break
+    return (best_student)
