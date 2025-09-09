38f1ea5eb2304b3f827a4fbc57dfd7f4a88442ab
Le-Meur-Mai <mai.lm@outlook.fr>
2025-09-09 15:34:40 +0200

    Deleting a space

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -6,7 +6,7 @@ def roman_to_int(roman_string):
     if roman_string is None or not isinstance(roman_string, str):
         return (0)
     roman_dictionary = {
-                            'I' : 1,
+                            'I': 1,
                             'V': 5,
                             'X': 10,
                             'L': 50,
