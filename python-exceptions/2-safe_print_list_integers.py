2c955829758282ce81661a2291184cc1a976e0f0
Le-Meur-Mai <mai.lm@outlook.fr>
2025-09-10 10:55:19 +0200

    Adding a function that prints the integers of a list

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+
+    try:
+        j = 0
+        for i in range(0, x):
+            if type(my_list[i]) is int:
+                print("{:d}".format(my_list[i]), end="")
+                j += 1
+        print("")
+        return (j)
+    except IndexError:
+        return (j)
