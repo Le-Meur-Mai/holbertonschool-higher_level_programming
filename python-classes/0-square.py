f49a3f87f5a34bfc1a028f9d2484452b6ab5d471
Le-Meur-Mai <mai.lm@outlook.fr>
2025-09-15 11:15:51 +0200

    Adding comments

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 
 '''
+0-square.py
+
 Code that define a class named Square
 '''
 
 
 class Square:
+
+    '''Initialization of a class that is named Square with all
+    of its attributes'''
+
     pass
