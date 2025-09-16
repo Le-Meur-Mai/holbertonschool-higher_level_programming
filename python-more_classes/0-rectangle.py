1430039debe08a8f38d095cd0b3085aa2b1cff25
Le-Meur-Mai <mai.lm@outlook.fr>
2025-09-16 10:20:19 +0200

    Adding a file that initialize an empty function and add the README file

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+'''
+0-rectangle.py
+
+Code that initialize an empty class Rectangle
+'''
+
+class Rectangle:
+    '''
+    initialization of the class Rectangle
+    '''
+    pass
