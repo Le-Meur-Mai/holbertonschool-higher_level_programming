7c4f35de32fc80b1d936edffa167c853d49ab416
Le-Meur-Mai <mai.lm@outlook.fr>
2025-09-22 10:50:55 +0200

    Adding a function that return a list of method and attributes an object can have

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/user/bin/python3
+
+'''0-lookup.py
+This module contain a function that can list all the attributes and methods of
+an object'''
+
+
+def lookup(obj):
+    '''This function return a list of method, and attributes an object can
+    have'''
+
+    class_attributes = dir(obj)
+
+    return class_attributes
---
+++ python-inheritance/README.md
@@ -0,0 +1,1 @@
+Python's exercises on the classes inheritance
