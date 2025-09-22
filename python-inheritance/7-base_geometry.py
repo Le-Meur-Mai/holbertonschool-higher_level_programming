848fa594899d3ca684792416d7eb85e8b61860ea
Le-Meur-Mai <mai.lm@outlook.fr>
2025-09-22 14:52:48 +0200

    Adding a function that tell you if an integer is valid or not

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''5-base_geometry.py
+Module that contain an empty class'''
+
+
+class BaseGeometry():
+    '''Creating an empty class with an empty method'''
+
+    def area(self):
+        '''Raising an exception'''
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        '''Raising exception if the integer is not valid'''
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        elif value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
