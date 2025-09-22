711f176f68c7a0735e67492154599b86c71b70e5
Le-Meur-Mai <mai.lm@outlook.fr>
2025-09-22 14:23:03 +0200

    Adding a method at a class that raise an exception message

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''5-base_geometry.py
+Module that contain an empty class'''
+
+
+class BaseGeometry():
+    '''Creating an empty class with an empty method'''
+    def area(self):
+        raise Exception("area() is not implemented")
