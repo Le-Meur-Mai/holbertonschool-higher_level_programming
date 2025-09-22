586d116818339867f34273d3e2117ce48bb2b9b4
Le-Meur-Mai <mai.lm@outlook.fr>
2025-09-22 15:51:41 +0200

    Importing the parent class

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,25 +2,7 @@
 '''8-base_geometry.py
 Module that contain an empty class'''
 
-
-class BaseGeometry():
-    '''Creating an empty class with an empty method'''
-
-    def area(self):
-        '''Raising an exception'''
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        '''Raising exception if the integer is not valid'''
-        if value is None:
-            raise TypeError(f"{name} must be an integer")
-        elif type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        elif value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-        else:
-            return True
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     '''Creating a subclass of BasicGeometry'''
