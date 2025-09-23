b0a3183f015d6bfc950baa9e1d70cf5e80e3fbb7
Le-Meur-Mai <mai.lm@outlook.fr>
2025-09-23 17:46:06 +0200

    Adding a subclass to list in order to customize its methods

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+'''task_02_verboselist.py
+Module that add a subclass to list, and customize its methods'''
+
+
+class VerboseList(list):
+    # Creating a subclass to list, in order to custom it
+    def append(self, item):
+        # Modification of the append method
+        super().append(item)
+        print(f"Added [{item}] to the list.")
+
+    def extend(self, list_item):
+        # Modification of the extend method
+        super().extend(list_item)
+        print(f"Extended the list with [{len(list_item)}] items.")
+
+    def remove(self, item):
+        # Modification of the remove method
+        if item < len(self) and item >= 0:
+            print(f"Removed [{item}] from the list.")
+        super().remove(item)
+
+    def pop(self, item=None):
+        # Modification of the pop method
+        if item is None:
+            print(f"Popped [{self[-1]}] from the list.")
+            super().pop()
+        elif item < len(self) and item >= 0:
+            print(f"Popped [{self[item]}] from the list.")
+            super().pop(item)
