f4ae53a976ac3f4d7620c05128248a227decc38e
Le-Meur-Mai <mai.lm@outlook.fr>
2025-09-23 09:56:45 +0200

    Adding a subclass of rectangle named square with it own methods

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''1-square.py
+Module that contain the subclass square'''
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''Creating the subclass of rectangle: square'''
+    def __init__(self, size):
+        '''Initialization of the size'''
+        if self.integer_validator("size", size) is True:
+            self.__size = size
+
+    def area(self):
+        '''Calculate the area of the square'''
+        return self.__size * self.__size
+
+    def __str__(self):
+        '''Print format of the square'''
+        value_printed = "[Rectangle] "
+        value_printed += str(self.__size)
+        value_printed += "/"
+        value_printed += str(self.__size)
+        return value_printed
