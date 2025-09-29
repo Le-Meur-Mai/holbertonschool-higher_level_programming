a4047c32f07905bbc8f6e967ee294db13cd0a6c5
Le-Meur-Mai <mai.lm@outlook.fr>
2025-09-29 14:17:32 +0200

    adding a new line

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -5,6 +5,7 @@
 Module that add a function who returns the dictionary description of an object
 '''
 
+
 def class_to_json(obj):
     '''Function that returns the dictionary description with simple data
     structure (list, dictionary, string, integer and boolean) for JSON
