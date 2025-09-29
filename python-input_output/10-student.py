52bee9ebfb01aa524e35ba4fdedc076ff09b9a6c
Le-Meur-Mai <mai.lm@outlook.fr>
2025-09-29 16:44:04 +0200

    Updating the method to_json of the class Student in order to get specifics values if specified in argument

---
+++ python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+'''
+10-student/py
+Module that add the class student with all its attributes and methods
+'''
+
+
+class Student:
+    '''Creating a class Student with all its attributes and methods'''
+
+    def __init__(self, first_name, last_name, age):
+        '''Initialization of the instance'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''Adding a method that retrieve the dictionnary format of all
+        the attributes of the instance or of the specified attributes'''
+        if attrs is None:
+            return self.__dict__
+        elif type(attrs) is list and len(attrs) > 0:
+            new_dict = {}
+            for i in attrs:
+                if type(i) is str and i in self.__dict__:
+                    new_dict[i] = self.__dict__[i]
+            if new_dict is {}:
+                return self.__dict__
+            else:
+                return new_dict
