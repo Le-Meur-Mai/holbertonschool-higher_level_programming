575dae5dafe4b0cc5b33af5025a8983c3e153e83
Le-Meur-Mai <mai.lm@outlook.fr>
2025-09-29 10:25:29 +0200

    Changing the comment for a docstring

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,7 +6,7 @@ Module that add a function that can read a file
 
 
 def read_file(filename=""):
-    # function that open and read a file
+    '''function that open and read a file'''
     with open(filename, "r", encoding='utf-8') as file:
         content = file.read()
-        print(content)
+        print(f"{content}", end="")
