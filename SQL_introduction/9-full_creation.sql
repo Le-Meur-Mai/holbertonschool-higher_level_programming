1543281b111b733115fc0abc920ebcf36c6ad123
Le-Meur-Mai <mai.lm@outlook.fr>
2025-10-20 16:05:07 +0200

    Adding a table with some base values in it

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- Creating a table and initialize some values in it
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+    );
+INSERT INTO second_table
+(id, name, score)
+VALUES (1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 8),
+(4, 'George', 8);
