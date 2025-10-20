2cfff0f960cf39d23e6c55f43b369b25c22d6072
Le-Meur-Mai <mai.lm@outlook.fr>
2025-10-20 16:22:31 +0200

    Adding a file that print two column of a table order by scores when the score is equal or superioir to 10

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,6 @@
+/*Script that display to columns, name and score of the table 'second-table' order in score,
+when the score is equal or superior to 10*/
+SELECT `score`, `name`
+FROM second_table
+WHERE `score` >= 10
+ORDER BY `score` DESC;
