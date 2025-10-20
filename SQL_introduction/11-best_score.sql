b05d8596c369146a7e7c73c638cdfc90085b2ed7
Le-Meur-Mai <mai.lm@outlook.fr>
2025-10-20 16:23:50 +0200

    Checker don't want a block comment

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,5 +1,5 @@
-/*Script that display to columns, name and score of the table 'second-table' order in score,
-when the score is equal or superior to 10*/
+-- Script that display two columns, name and score of the table 'second-table' order in score, when the score is equal or superior to 10
+
 SELECT `score`, `name`
 FROM second_table
 WHERE `score` >= 10
