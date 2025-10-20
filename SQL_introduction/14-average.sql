b5ef967929f3faa466c93a149bc1a4800c839f7e
Le-Meur-Mai <mai.lm@outlook.fr>
2025-10-20 17:06:36 +0200

    Correcting the code wich was wrong

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,6 +1,2 @@
 -- Return the score average of all the data in second_table
-ALTER TABLE second_table
-ADD COLUMN average FLOAT;
-@average_score = (SELECT AVG(`score`) FROM second_table);
-UPDATE second_table
-SET `average` = @average_score
+SELECT AVG(score) AS average FROM second_table;
