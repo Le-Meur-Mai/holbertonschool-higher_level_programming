4fac1054cbca421a23be85054e95e77785cf5b0b
Le-Meur-Mai <mai.lm@outlook.fr>
2025-10-20 14:53:31 +0200

    Script that add a table in a given database

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- Script that create a table in a database passed in argument if it doesn't already exists
+CREATE TABLE IF NOT EXISTS first_table (
+        id INT,
+        name VARCHAR(256)
+        );
