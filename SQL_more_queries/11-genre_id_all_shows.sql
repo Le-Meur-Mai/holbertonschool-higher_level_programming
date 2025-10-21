0a3a44c0f67f52ffcaa6ea1eeea82acf1132d728
Le-Meur-Mai <mai.lm@outlook.fr>
2025-10-21 16:53:05 +0200

    delet IFNULL statement

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- Script that display the title of the show and the genre_id in ascending order, write NULL if genre is none
-SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, "NULL") as genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
