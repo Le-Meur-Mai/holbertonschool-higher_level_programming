ceb52eec371f943a259714cc5c94050b464ea1b4
Le-Meur-Mai <mai.lm@outlook.fr>
2025-10-21 14:53:17 +0200

    Adding a script that put the id to 1 per default

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
--- Script that create a table where the column can't be NULL
+-- Script that create a table where the column name can't be NULL
 CREATE TABLE IF NOT EXISTS `force_name`(
     id INT,
     name VARCHAR(256) NOT NULL
-)
+);
