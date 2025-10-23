6c224bf4ff6f560c15e3fd88e8ad9c63324ad522
Le-Meur-Mai <mai.lm@outlook.fr>
2025-10-23 10:54:27 +0200

    Adding quote in the output

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -20,6 +20,6 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("SELECT id, name FROM states ORDER BY id")
     for row in cur.fetchall():
-        print(f"({row[0]}, {row[1]})")
+        print(f"({row[0]}, '{row[1]}')")
     cur.close()
     db.close()
