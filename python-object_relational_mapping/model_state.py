958304062864688f9c485b3c93c63162340dc63c
Le-Meur-Mai <mai.lm@outlook.fr>
2025-10-24 10:30:16 +0200

    Adding a script that create a python class related to a table thanks to mysqlAlchemy

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""model_state.py
+Creation of the class state for SQLAlchemy in order to use ORM"""
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+import sys
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column('states.id', Integer, primary_key=True, autoincrement=True)
+    name = Column('states.name', String(128), nullable=False)
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+    Base.metadata.create_all(engine)
