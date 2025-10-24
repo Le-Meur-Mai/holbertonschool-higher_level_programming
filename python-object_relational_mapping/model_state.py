35ee49369b8724fdb3151c13d5cd93f0222bce91
Le-Meur-Mai <mai.lm@outlook.fr>
2025-10-24 10:36:30 +0200

    Deleting some code who wasn't in the adequate file

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """model_state.py
 Creation of the class state for SQLAlchemy in order to use ORM"""
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-import sys
 
 Base = declarative_base()
 
@@ -13,15 +12,3 @@ class State(Base):
     __tablename__ = 'states'
     id = Column('states.id', Integer, primary_key=True, autoincrement=True)
     name = Column('states.name', String(128), nullable=False)
-
-
-if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(
-            sys.argv[1],
-            sys.argv[2],
-            sys.argv[3]
-        ),
-        pool_pre_ping=True
-    )
-    Base.metadata.create_all(engine)
