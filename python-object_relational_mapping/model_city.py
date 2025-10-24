03fc64617d557969d87378ba0063b4dd82b1fd2c
Le-Meur-Mai <mai.lm@outlook.fr>
2025-10-24 15:40:09 +0200

    Adding a module that create a session and request all the cities and their informationsfrom the table City

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""model_state.py
+Creation of the class state for SQLAlchemy in order to use ORM"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+class City(Base):
+    '''Class that represents the table city in a database'''
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
