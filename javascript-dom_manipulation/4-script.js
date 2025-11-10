c15fbfa0a8113d2fcf4e93da3383c64f9e8074f8
Le-Meur-Mai <mai.lm@outlook.fr>
2025-11-10 13:55:40 +0100

    Updating the script so that ul element with that class are affected by this script

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -3,7 +3,7 @@
 /* script that adds a li element to a list when the user clicks on the element with id add_item */
 
 function AddLi () {
-  const listToChange = document.querySelector('.my_list');
+  const listToChange = document.querySelector('ul.my_list');
   const newElement = document.createElement('li');
   newElement.textContent = 'Item';
   listToChange.appendChild(newElement);
