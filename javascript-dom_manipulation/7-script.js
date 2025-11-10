836ab51117c35582207ce4bd088771c46eeaa23a
Le-Meur-Mai <mai.lm@outlook.fr>
2025-11-10 15:16:21 +0100

    Fix semistandard style

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -2,15 +2,15 @@
 
 /* script that fetches and lists the title for all movies */
 
-url = 'https://swapi-api.hbtn.io/api/films/?format=json'
+const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
 
 fetch(url)
-.then(response => response.json())
-.then(data => {
+  .then(response => response.json())
+  .then(data => {
     const listMovies = document.querySelector('ul#list_movies');
     data.results.forEach(movie => {
       const newElement = document.createElement('li');
       newElement.textContent = movie.title;
       listMovies.appendChild(newElement);
     });
-})
+  });
