27989f0325accba34ed1bd3a7924ba980b92f2cf
Le-Meur-Mai <mai.lm@outlook.fr>
2025-11-13 10:04:20 +0100

    Print the error and not raising error

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -2,32 +2,35 @@
 
 import os
 
-''' a Python function that generates personalized invitation files from a
-template with placeholders and a list of objects.
-Arguments:
-template: text with placeholders
-attendees: a list of dictionaries
-return: Error on failure, nothing on success.'''
-
 
 def generate_invitations(template, attendees):
+    ''' a Python function that generates personalized invitation files from a
+    template with placeholders and a list of objects.
+    Arguments:
+    template: text with placeholders
+    attendees: a list of dictionaries
+    return: Error on failure, nothing on success.'''
 
     if not isinstance(template, str) or not isinstance(attendees, list):
-        raise TypeError('template must be a string and attendees a list of'
-                        'dictionaries.')
+        print('template must be a string and attendees a list of'
+              'dictionaries.')
+        return
 
     elif len(template) <= 0:
-        raise ValueError('Template is empty, no output files generated.')
+        print('Template is empty, no output files generated.')
+        return
 
     elif len(attendees) <= 0:
-        raise ValueError('No data provided, no output files generated.')
+        print('No data provided, no output files generated.')
+        return
 
     dict_model = ['name', 'event_title', 'event_date', 'event_location']
 
     for invite in attendees:
         if not isinstance(invite, dict):
-            raise ValueError('template must be a string and attendees a list'
-                             ' of dictionaries.')
+            print('template must be a string and attendees a list'
+                  ' of dictionaries.')
+            return
         for key in dict_model:
             if key not in invite or invite[key] is None:
                 invite[key] = 'N/A'
@@ -35,14 +38,9 @@ def generate_invitations(template, attendees):
         i = 0
         for attendee in attendees:
             i += 1
-            if os.path.exists(f'output_{i}.txt'):
-                continue
             with open(
                     f'output_{i}.txt', 'w', encoding='utf-8') as invitation:
-                invite = template
-                for key in attendee:
-                    invite = invite.replace(f'{{{key}}}', attendee[key])
-                invitation.write(invite)
+                invitation.write(template.format(**attendee))
 
     except Exception:
         raise
