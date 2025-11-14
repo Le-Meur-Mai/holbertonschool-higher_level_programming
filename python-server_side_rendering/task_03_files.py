b951e2acdd9e62079b29400eb5ab3ad96c7c8c7f
Le-Meur-Mai <mai.lm@outlook.fr>
2025-11-14 23:55:27 +0100

    Adding the progress on the task that retrieve parameters from the url

---
+++ python-server_side_rendering/task_03_files.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+
+from flask import Flask, render_template, request
+import json
+import csv
+
+'''Module that use jinja for reusable html code, and load all the data in
+the server side'''
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/items')
+def items():
+    try:
+        with open('items.json', 'r', encoding='utf-8') as file:
+            list_items = json.load(file)
+            items=list_items['items']
+    except:
+        items = []
+    return render_template('items.html', items=items)
+
+@app.route('/products')
+def products():
+
+    file = request.args.get('source')
+    id = request.args.get('id')
+
+    if file == 'json':
+        try:
+            with open('products.json', 'r', encoding='utf-8') as json_content:
+                data = json.load(json_content)
+        except Exception as error:
+            print(error)
+
+    elif file == 'csv':
+        try:
+            with open('products.csv', 'r', encoding='utf-8') as csv_content:
+                csv_reader = csv.DictReader(csv_content)
+                data = []
+                for row in csv_reader:
+                    data.append(row)
+        except Exception as error:
+            print(error)
+ 
+    else:
+        print('Wrong source')
+        return
+    
+    if id:
+        try:
+            i = 0
+            for product in data:
+                if product.get('id') == id:
+                    data = [product]
+                    i += 1
+                    break
+            if i == 0:
+                data = None
+        except Exception as error:
+            print(error)
+
+    return render_template('product_display.html', data=data)
+    
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
